073e369457b81c0e6c0558c9d0e9e3f20c9d767f
zlobin_ilya <qwertin1998@gmail.com>
2023-02-19 13:24:04 +0300

    Add: admin guard

---
+++ backend/src/auth/is-admin-guard.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class IsAdminGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    try {
+      const user = req.user;
+      console.log(user)
+      return user.isAdmin;
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+  }
+}
